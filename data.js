3e147b05a2c3f7729c5e395f509cb709767a09e3
creatorsteam001 <133876995+creatorsteam001@users.noreply.github.com>
2023-05-20 13:22:43 +0530

    Add files via upload

---
+++ data.js
@@ -0,0 +1,28 @@
+let movies = [
+    {
+        name: 'Loki',
+        des: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit porro et veniam excepturi, eaque voluptatem impedit nulla laboriosam facilis ut laboriosam libero!',
+        image: 'image/SLIDER 1.jpg'
+    },
+    {
+        name: 'Falcon And The Winter Soldier',
+        des: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit porro et veniam excepturi, eaque voluptatem impedit nulla laboriosam facilis ut laboriosam libero!',
+        image: 'image/SLIDER 2.jpg'
+    },
+    {
+        name: 'Wanda Vision',
+        des: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit porro et veniam excepturi, eaque voluptatem impedit nulla laboriosam facilis ut laboriosam libero!',
+        image: 'image/SLIDER 3.jpg'
+    },
+    {
+        name: 'RAVA',
+        des: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit porro et veniam excepturi, eaque voluptatem impedit nulla laboriosam facilis ut laboriosam libero!',
+        image: 'image/SLIDER 4.jpg'
+    },
+    {
+        name: 'lUCA',
+        des: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit porro et veniam excepturi, eaque voluptatem impedit nulla laboriosam facilis ut laboriosam libero!',
+        image: 'image/SLIDER 5.jpg'
+    },
+/// and so on
+]
